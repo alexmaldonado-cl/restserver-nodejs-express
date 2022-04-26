2aa7e1bfb4b8d3de9376f0ffc78d82782644b259
Alex Maldonado <alex.maldonado@outlook.com>
2022-04-25 21:56:45 -0400

    Configuración de Routes & Controllers

--- models/server.js
+++ models/server.js
@@ -1,24 +1,30 @@
 const express = require('express');
+const cors    = require('cors');
 
 class Server {
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios';
 
+        //Middlewares
         this.middlewares();
 
+        //Rutas de mi aplicación
         this.routes();
     }
 
     middlewares() {
+        //CORS
+        this.app.use(cors())
+
+        //Directorio Público
         this.app.use( express.static('public') );
     }
 
     routes() {
-        this.app.get('/', (req, res) => {
-            res.send('Hello World!');
-        });
+        this.app.use(this.usuariosPath, require('../routes/usuarios'));
     }
 
     listen() {
