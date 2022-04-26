269431513ebb633c680d33d4ee0def5621050a5d
Alex Maldonado <alex.maldonado@outlook.com>
2022-04-25 23:14:14 -0400

    Configuración de Responses en controlador de usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,21 +1,34 @@
 const { response } = require('express');
 
 const usuariosGet = (req, res = response) => {
+    const {s, nombre = 'Anonimo', page = 1, limit = 10} = req.query;
     res.json({
-        msg: 'get API - Controller'
+        msg: 'get API - Controller',
+        s,
+        nombre,
+        page,
+        limit
     });
 };
 
 const usuariosPost = (req, res = response) => {
+    const {nombre, edad} = req.body;
     res.json({
-        msg: 'post API - Controller'
+        msg: 'post API - Controller',
+        nombre,
+        edad
     });
 };
 
 
 const usuariosPut = (req, res = response) => {
+
+    const {id} = req.params;
+
+
     res.json({
-        msg: 'put API - Controller'
+        msg: 'put API - Controller',
+        id
     });
 };
 
--- models/server.js
+++ models/server.js
@@ -17,7 +17,10 @@ class Server {
 
     middlewares() {
         //CORS
-        this.app.use(cors())
+        this.app.use(cors());
+
+        //Lectura y parseo del body
+        this.app.use(express.json());
 
         //Directorio Público
         this.app.use( express.static('public') );
